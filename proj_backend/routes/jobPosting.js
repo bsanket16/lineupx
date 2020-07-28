357ace474cfd6d8431cbf030248bd165e18b87e3
sankt <bsankt16@gmail.com>
2020-07-28 12:15:17 +0530

    deploy 39

--- proj_backend/routes/jobPosting.js
+++ proj_backend/routes/jobPosting.js
@@ -18,7 +18,7 @@ router.param('jobId', getJobPostingById)
 router.post('/job-postings/create',[
     //check
     check('title').not().isEmpty().withMessage('Job title required').isLength({ min : 3 }).withMessage('Job title too short').isLength({ max : 30 }).withMessage('Job title too long'),
-    check('companyName').not().isEmpty().withMessage('Company name required').isLength({ min : 3 }).withMessage('Company name too short').isLength({ max : 15 }).withMessage('Company name too long'),
+    // check('companyName').not().isEmpty().withMessage('Company name required').isLength({ min : 3 }).withMessage('Company name too short').isLength({ max : 15 }).withMessage('Company name too long'),
     check('location').isLength({ min : 3}).withMessage('Location not found'),
     check('openings').not().isEmpty().withMessage('Number of openings required'),
     check('salary').not().isEmpty().withMessage('Salary cannot be empty')
